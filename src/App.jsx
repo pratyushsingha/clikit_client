3bf81baf2ceef13f721c7ea41fda6ad5ae078ba9
pratyushsingha <pratyushsingha83@gmail.com>
2024-04-01 15:33:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./index.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
